fa29f99122377a45de04ed13900e5be12001cc6e
Oskar Veileborg <ohv1020@hotmail.com>
2013-05-09 23:33:58 +0200

    Removed some shitty code
    The whole customTile affair was custom-designed for a project I was
    working on a while back, it doesn't have a place in the snippet-pack.

--- snippets/drawable.hpp
+++ snippets/drawable.hpp
@@ -13,7 +13,6 @@ namespace bzsf {
 		
 		bzsf::tsTile * texTile;
 		bzsf::Animation anim;
-		bool customTile;
 
 	public:
 		bzsf::tsTile * GetTile()		{return texTile;}
@@ -41,14 +40,14 @@ namespace bzsf {
 			dType = animation;
 		}
 
-		void SetTexture(sf::Texture * tex) {
+		void SetTexture(sf::Texture& tex) {
 			float xPos = entity.getPosition().x;
 			float yPos = entity.getPosition().y;
+
+			entity.setTexture(tex);
 			
-			entity.setTexture(*tex, true);
-
 			entity.setPosition(xPos, yPos);
-
+			
 			dType = texture;
 		}
 
@@ -66,58 +65,27 @@ namespace bzsf {
 			dType = tile;
 		}
 
-		void NewCRegion(int width, int height) {
-			if(dType == tile) {
-				if(!customTile) {
-					int txOff = texTile->xOffset;
-					int tyOff = texTile->yOffset;
-					int tWidth = texTile->width;
-					int tHeight = texTile->height;
-					sf::Texture * tTex = texTile->texture;
-
-					texTile = new bzsf::tsTile();
-					customTile = 1;
-
-					texTile->xOffset = txOff;
-					texTile->yOffset = tyOff;
-					texTile->texture = tTex;
-				}
-				texTile->width = width;
-				texTile->height = height;
-
-				dType = tile;
-			} else {std::cerr << "Tried to assign new Collision Region to an entity without a tile." << std::endl;}
-		}
-
-
-		Drawable() : customTile(false), texTile(0) {
-
+		Drawable() : texTile(0) {
 			dType = none;
 		}
 
-		explicit Drawable(bzsf::Animation& animation) : customTile(false), texTile(0) {
+		explicit Drawable(bzsf::Animation& animation) : texTile(0) {
 			SetAnimation(animation);
 		}
 
-		explicit Drawable(sf::Texture& texture) : customTile(false), texTile(0) {
+		explicit Drawable(sf::Texture& texture) : texTile(0) {
 			SetTexture(&texture);
 		}
 
-		explicit Drawable(tsTile * tile) : customTile(false), texTile(0) {
+		explicit Drawable(tsTile * tile) : texTile(0) {
 			SetTile(tile);
 		}
 
 		virtual ~Drawable() {
-			if(customTile) {
-				delete texTile;
-			}
-
 			texTile = 0;
 		}
 	};
 
-} //ENDOF NAMESPACE bzsf
-
-//void bzsf::Animation::SetFrame(int index, Drawable& entity, bool speedToZero = false) 
+} //ENDOF NAMESPACE bzsf 
 
 #include <BZeps-SFML-Snippets\snippets\drawable\particleSystem.hpp>
