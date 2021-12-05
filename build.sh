ee04c777d25553d2879e22f67dd4adff5d0f33e5
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2021-12-05 21:11:12 +0100

    Build library statically instead

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #! /usr/bin/env -S bash -euo pipefail
-g++ -g -O2 -fPIC $(find src -name "*.cpp") -I. -shared -o lib/libbzsf.so
+
+FILES=$(find src -name "*.cpp")
+mkdir -p build
+
+for f in $FILES; do
+	g++ -c -g -O2 -I. $f -o "build/$(basename ${f%.cpp}.o)"
+done
+
+ar rcs lib/libbzsf.a build/*.o
+
+# We can also compile a shared library?
+# g++ -g -O2 -fPIC $FILES -I. -shared -o lib/libbzsf.so
+
 echo Success
 ls lib
